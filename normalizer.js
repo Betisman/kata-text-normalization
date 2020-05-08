26fd44b2bfb4fbec396ae73e41f7ea5c3d8124e4
Nuria Extremadouro Mayoral <nuriaextremadouromayoral@MacBook-Pro-de-Nuria.local>
2020-05-08 16:40:44 +0200

    Remove diacriticals

--- normalizer.js
+++ normalizer.js
@@ -1,3 +1,6 @@
 module.exports = language => input => {
-    return input;
+    const diacriticalRemoval = word => word.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    return input.split(' ')
+        .map(word => word.toUpperCase())
+        .map(diacriticalRemoval);
 };
--- text-normalization.test.js
+++ text-normalization.test.js
@@ -2,8 +2,21 @@ const expect = require('expect.js');
 const normalizer = require('./normalizer');
 
 describe("text normalization", () => {
-    it('should return the same word', () => {
-        const output = normalizer('ES')('casa');
-        expect(output).to.equal('casa');
-    })
+    describe("spanish normalizer", () => {
+        const spanish = normalizer('ES');
+        it('should return the same word uppercase', () => {
+            const output = spanish('casa');
+            expect(output).to.eql(['CASA']);
+        });
+    
+        it('should return an array splitted by space with the same words', () => {
+            const output = spanish('casa blanca');
+            expect(output).to.eql(['CASA', 'BLANCA']);
+        });
+    
+        it('should remove all accents', () => {
+            const output = spanish('casá blanca');
+            expect(output).to.eql(['CASA', 'BLANCA']);
+        });
+    });
 });
